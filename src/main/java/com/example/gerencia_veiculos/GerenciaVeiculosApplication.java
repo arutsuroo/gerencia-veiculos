4de1e51432856c741ffea52fa72002c150434818
Arthur Oliveira <arthur2109.vieira@gmail.com>
2026-01-29 16:00:23 -0300

    [TASK-001] - Geração do projeto

---
+++ src/main/java/com/example/gerencia_veiculos/GerenciaVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerencia_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaVeiculosApplication.class, args);
+	}
+
+}
